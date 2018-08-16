85cc758baf7d68d617637c18f76f1c10f7af330d
Gihan Uthpala Panapitiya <gihanuthpala@gmail.com>
2018-08-16 15:21:08 -0400

    Update all_sub.py

--- all_sub.py
+++ all_sub.py
@@ -1,13 +1,6 @@
-from numpy import *
-import numpy
 from itertools import combinations
-import pickle
-import json as simplejson
 import numpy as np
-from array import *
 from random import randint
-from subprocess import call
-import subprocess
 import os
 
 
@@ -51,11 +44,12 @@ new=list(combinations(goldpositions[:,0], numsubs))
 
 positions = np.array(new)
 
-	
+xyzname = "input.xyz"
 for i in range(0,numruns):
-  g = open("input.bas","w")
-  basname = "input.bas"
-  filedir = str(i)
+  filedir = str(i)  
+  os.system("mkdir"+" "+filedir)
+  g = open(filedir+"/"+xyzname,"w")
+  
 
   g.write(str(natoms)+'\n') 
   for j in range(0,int(natoms)):
@@ -66,55 +60,4 @@ for i in range(0,numruns):
       g.write("%d \t %f \t %f \t %f  \n" % (data[j][0] , data[j][1],  data[j][2],  data[j][3]))
       
   g.close
-
-  os.system("mkdir"+" "+str(i))
-  os.system("mv"+" "+basname+" "+str(i))
-  os.system("cd"+" "+filedir)
-
-  os.system("ln -s ../fireball.x"+" "+filedir)
-  os.system("ln -s ../Fdata"+" "+filedir)
-  os.system("ln -s ../rms.input"+" "+filedir)
-  os.system("ln -s ../fireball.in"+" "+filedir)
-
-jobfile = open("gold.job","w")
-jobfile.write("#!/bin/bash\n")
-jobfile.write("#PBS -q standby\n")
-jobfile.write("#PBS -l nodes=1:ppn=1\n")
-jobfile.write("#PBS -l walltime=4:00:00\n")
-jobfile.write("#PBS -t 0-"+str(numruns-1)+"\n")
-jobfile.write("#PBS -m ae\n")
-jobfile.write("#PBS -l pvmem=4gb\n")
-jobfile.write("#PBS -e pathToErrorFile\n")
-jobfile.write("#PBS -M gihansflying@gmail.com\n")
-jobfile.write("#PBS -N au"+str(nauatoms)+"-"+str(numsubs)+"-${PBS_ARRAYID}\n")
-jobfile.write("\n")
-jobfile.write("source ~/.bashrc\n")
-jobfile.write("\n")
-jobfile.write(" cd "+workdir+"/"+"${PBS_ARRAYID}"+"\n")
-
-jobfile.write("    ./fireball.x > output.log\n")
-
-jobfile.close()
-
-#CREATING FIREBALL.IN   
-fireinfile = open("fireball.in","w")
-fireinfile.write("&OPTION\n")
-fireinfile.write("basisfile = "+basname+"\n") 
-fireinfile.write("ihorsfield = 1\n")
-fireinfile.write("imcweda = 0\n")
-fireinfile.write("iquench = -3\n")
-fireinfile.write("icluster = 1\n")
-fireinfile.write("nstepf = 5000\n")
-fireinfile.write("qstate = 1.0\n")
-fireinfile.write("dt = 0.5\n")
-fireinfile.write("&END\n")
-fireinfile.write("&OUTPUT\n")
-fireinfile.write("iwrtxyz = 1\n")
-fireinfile.write("iwrtcharges = 1\n")
-fireinfile.write("iwrteigen = 1\n")
-fireinfile.write("&END\n")
-   
-fireinfile.close()
-
-
 
